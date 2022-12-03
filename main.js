fade94c80c1d48b788301529d4b7119da8da1c78
Aryakoste <aryakhochare@gmail.com>
2022-12-03 08:17:51 +0530

    commit

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
+let menuDropArr = ["Overview", "Projects", "Campaigns", "Followers"];
+
 function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
 }
+
+function myFunction2() {
+  document.getElementById("myDropdown2").classList.toggle("show");
+  document.getElementById("myDropdown2").classList.add("show")
+}
   
-  // Close the dropdown if the user clicks outside of it
+
   window.onclick = function(event) {
     if (!event.target.matches('.select-drp')) {
       var dropdowns = document.getElementsByClassName("dropdown-content");
@@ -14,6 +21,45 @@ function myFunction() {
         }
       }
     }
+}
+
+let sidebarStyle = document.querySelector('.sidebar').style.display;
+
+const openSidebar = () => {
+   sidebarStyle = "block";
+}
+
+document.querySelector('main').addEventListener('click', () => {
+  if(!sidebarStyle == "none"){
+    document.querySelector('.sidebar').style.display = "none";
   }
+})
+
+menuDropArr.map((data) => {
+    let div = document.createElement('div');
+    div.innerText = data;
+    let menuItem = document.querySelector('.menu-drop-items-1');
+    menuItem.appendChild(div);
+})
+
+document.querySelector('.item-1').addEventListener('click', () => {
+      var el = document.querySelector('.menu-drop-items-1');
+      console.log(el.classList)   
+      if(!el.classList.contains('active'))
+          el.classList.add('active');
+      else
+          el.classList.remove('active');
+})
+
+document.querySelector('.menu-icon').addEventListener('click', () => {
+      var el = document.querySelector('.sidebar');
+      el.classList.toggle('show');
+});
+
+document.querySelector('.close-side').addEventListener('click', () => {
+  var el = document.querySelector('.sidebar');
+  el.classList.toggle("show");
+});
+
 
 
